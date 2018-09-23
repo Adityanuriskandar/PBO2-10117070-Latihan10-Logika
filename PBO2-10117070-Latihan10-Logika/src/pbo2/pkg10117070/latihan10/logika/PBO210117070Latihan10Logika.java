ec9042905f09bbce673b1411059330c0585caedc
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-09-23 12:45:32 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan10-Logika/src/pbo2/pkg10117070/latihan10/logika/PBO210117070Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan10.logika;
+
+/**
+ *
+ * @author Aditya
+ * NAMA     : Aditya Nur Iskandar
+ * KELAS    : PBO2/IF2
+ * NIM      : 10117070
+ * Deskripsi Program : Program ini berisi program untuk mengetahui penggunaan
+ * Tipe data Boolean.
+ */
+public class PBO210117070Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "+dewasa);
+    }
+    
+}
